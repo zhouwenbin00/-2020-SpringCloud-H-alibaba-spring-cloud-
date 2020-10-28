d0737c4d1c1f1e8dd0afa2720d3d2910e8650cfa
zhouwenbin <2825075112@qq.com>
2020-10-28 21:13:30 +0800

    11_热部署Devtools

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -27,13 +27,13 @@ public class PaymentController {
     }
 
     @GetMapping(value = "/payment/get/{id}")
-    public CommonResult getPaymentById(@PathVariable Long id){
+    public CommonResult<Payment> getPaymentById(@PathVariable Long id){
         Payment result = paymentService.getPaymentById(id);
         log.info("***********查询结果：{}", result);
         if (result != null){
-            return new CommonResult(200,"查询成功", result);
+            return new CommonResult<>(200,"查询成功", result);
         }else {
-            return new CommonResult(444,"查询失败", null);
+            return new CommonResult<>(444,"查询失败", null);
         }
     }
 }
