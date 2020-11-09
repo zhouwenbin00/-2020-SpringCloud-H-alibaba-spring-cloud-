d2f4393f166b1e98f83d1c5c54a2d0a2d2a2f95e
zhouwenbin <2825075112@qq.com>
2020-11-09 21:24:17 +0800

    33_服务提供者注册进Consul

---
+++ cloud-providerconsul-payment8006/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,27 @@
+package com.atguigu.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@Slf4j
+@RestController
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    /**
+     * http://localhost:8006/payment/zk
+     *
+     * @return
+     */
+    @RequestMapping(value = "/payment/consul", method = RequestMethod.GET)
+    public String paymentZk() {
+        return "SpringCloud with consul:" + serverPort + "\t" + UUID.randomUUID().toString();
+    }
+}
